b5b39a54c0c66ffc3f0331af66dc00bde26b5561
Jolo Gerard  Miel Fabros Tolentino <97835603+JoloTolentino@users.noreply.github.com>
2022-11-29 14:16:04 -0500

    Time: 3586 ms (84.97%), Space: 112.9 MB (5.35%) - LeetHub

---
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -0,0 +1,33 @@
+class Solution:
+    def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
+        
+        
+        row,col = len(grid1),len(grid1[0])
+        
+        def dfs(y,x):
+            
+            directions = ((y+1,x),(y-1,x),(y,x+1),(y,x-1))
+            
+            if grid2[y][x] ==0:
+                return True
+            if grid2[y][x] ==1 and grid1[y][x] != grid2[y][x]:
+                return False
+            if grid2[y][x] == 1 and grid1[y][x] == grid2[y][x]:
+                grid1[y][x]= 0
+                grid2[y][x] = 0 
+                
+                temp = [] 
+                for move in directions:
+                    if 0<=move[0]<row and 0<=move[1]<col:
+                        temp.append(dfs(move[0],move[1]))
+            
+            return all(temp)
+            
+    
+        count =0 
+        for y in range(row):
+            for x in range(col):
+                if grid2[y][x] == 1 and dfs(y,x): 
+                    count+=1
+        print(grid2)
+        return count

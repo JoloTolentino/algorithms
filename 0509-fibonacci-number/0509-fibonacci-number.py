bed9e361def353d3bb89087a20f9dda13db26a42
Jolo Gerard  Miel Fabros Tolentino <97835603+JoloTolentino@users.noreply.github.com>
2022-12-01 22:43:46 -0500

    Time: 60 ms (49.73%), Space: 13.9 MB (55.30%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,14 +1,31 @@
 class Solution:
     def fib(self, n: int) -> int:
-    
-        if n == 0:
-            return 0 
-        if n ==1:
-            return 1
-
-        return self.fib(n-1)+ self.fib(n-2)
-
+        
+        
+        
+        seen = {0:0,1:1}
+        
+        
+        def memoization(val=2):
+            if val not in seen:
+                seen[val] = seen[val-1]+seen[val-2]
             
+            if val!= n:
+                memoization(val+1)
+                
+                
+        
+        if n <=1:
+            return seen[n]
+        
+        else:
+            memoization()
+            return seen[n]
+                
+                
+        
+        
+        
             
             
             

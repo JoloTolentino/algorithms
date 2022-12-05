36c486c03949cbdfc3ed2573114ec806609b5299
Jolo Gerard  Miel Fabros Tolentino <97835603+JoloTolentino@users.noreply.github.com>
2022-12-05 16:22:04 -0500

    Time: 1082 ms (15.09%), Space: 16.4 MB (66.11%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,11 +3,10 @@ class Solution:
         row,col  = len(grid),len(grid[0])
         
         def dfs(y,x):
-    
+            directions= ((y+1,x),(y-1,x),
+                         (y,x+1), (y,x-1))
             if 0<= y<row and 0<=x< col and grid[y][x]=='1': 
                 grid[y][x] = '0'
-                directions= ((y+1,x),(y-1,x),
-                             (y,x+1), (y,x-1))
                 for direction in directions: 
                     dfs(direction[0],direction[1])
             
